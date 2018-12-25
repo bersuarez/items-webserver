6742b1baf59f9a764e17ad449d7f6ad64de436c7
Bernardo Suarez Sepulveda <b@xhash.tech>
2018-12-24 23:45:36 -0600

    added post request, fixed ports, added spanish version

--- webserver.py
+++ webserver.py
@@ -1,35 +1,78 @@
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer 
+from http.server import BaseHTTPRequestHandler, HTTPServer 
+import cgi
 
-#extend class
-class webserverHandler(BaseHTTPRequestHandler) 
+class webserverHandler(BaseHTTPRequestHandler): 
     def do_GET(self):
         try:
             if self.path.endswith("/hello"):
                 self.send_response(200)
                 self.send_header('Content-type','text/html')
                 self.end_headers()
-
                 output = ""
-                output += "<html><body>Hello!</body><html>"
-                self.wfile.write(output)
-                print output
+                output += "<html><body>"
+                output += "<h1>Hello!</h1>"
+                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+                output += "</body></html>"
+                self.wfile.write(output.encode())
+                print(output)
                 return
+
+            if self.path.endswith("/hola"):
+                self.send_response(200)
+                self.send_header('Content-type','text/html')
+                self.end_headers()
+                output = ""
+                output += "<html><body>"
+                output += "<h1>&#161 Hola !</h1>"
+                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+                output += "</body></html>"
+                self.wfile.write(output.encode())
+                print(output)
+                return
+
         except IOError:
             self.send_error(404,"File Not Found %s" % self.path)
+
+    def do_POST(self):
+        print("postando")
+        try:
+            self.send_response(301)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            print("paso1")
+            ctype, pdict = cgi.parse_header(
+                self.headers.getheader('content-type'))
+            if ctype == 'multipart/form-data':
+                fields = cgi.parse_multipart(self.rfile, pdict)
+                messagecontent = fields.get('message')
+            print("paso 2")
+            output = ""
+            output += "<html><body>"
+            output += " <h2> Okay, how about this: </h2>"
+            output += "<h1> %s </h1>" % messagecontent[0]
+            output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+            output += "</body></html>"
+            self.wfile.write(output.encode())
+            print(output)
+
+        except:
+            pass
+
 
 
 def main():
     try:
-        port = 8080
-        server = HTTPServer((''),webserverHandler)
-        print "Web server running on port %s" %port
-        server.serve_forever() 
-
-
+        port=8080#redirected to 8085 in vagrant file
+        server_address = ('', port)
+        print("Web server running on port %s" %port)
+        httpd = HTTPServer(server_address, webserverHandler)
+        httpd.serve_forever() 
     except KeyboardInterrupt: #when user hold ^C
-        print "^C entered, stopping web server..."
-        server.socket.close()
+        print("^C entered, stopping web server...")
+        httpd.socket.close()
+
 
 #immediately run the main method when Python interpreter executes scrypt        
-if __main__ == '__main__':
+if __name__ == '__main__':
     main()
+
